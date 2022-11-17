4eec7d3fee5e5c917c63c08862f7b15214aba0ab
chunchia.liang <chunchia.liang61@titansoft.com.sg>
2022-11-17 14:07:12 +0800

    first commit

---
+++ ConsoleApp3/Bmi.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static class BMI
+{
+    public static string Bmi(double height, double weight)
+    {
+        var bmi = BmiCounter(height, weight);
+        return GetBmiResultOutput(bmi);
+    }
+
+    private static double BmiCounter(double height, double weight)
+    {
+        return weight / (height * height);
+    }
+
+    private static string GetBmiResultOutput(double bmi)
+    {
+        return bmi switch
+        {
+            <= 18.5 => "Underweight",
+            <= 25.0 => "Normal",
+            <= 30.0 => "Overweight",
+            > 30 => "Obese",
+            _ => ""
+        };
+    }
+} 
+
